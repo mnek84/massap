a136053b68235cd897b9022bad0f580f92a871d8
Maximiliano <mnek84@gmail.com>
2015-03-13 15:05:24 -0300

    FIRST COMMIT

---
+++ www/js/index.js
@@ -0,0 +1,74 @@
+var app = {
+    // Application Constructor
+    isAndroid:false,
+    apiServer:"http://ion.nonstoptv.tv/",
+    initialize: function() {        
+        this.bindEvents();
+        //Inicio de APP        
+        if (deviceDetect.Android()!=null)this.isAndroid=true;        
+        this.showLoadingBar("Iniciando Aplicación");
+        
+    },
+    // Bind Event Listeners
+    bindEvents: function() {
+        
+        $(function() {
+            
+            document.addEventListener("deviceready", app.onDeviceReady, false);
+            
+        });        
+        
+    },
+    cargarNotas:function(categoria){
+        this.showLoadingBar("Cargando Noticias");
+        $.getJSON(this.apiServer + 'rss/parser.php', function(data) {
+
+        var source   = $("#rss-template").html();
+        var template = Handlebars.compile(source);
+
+        $.each(data, function (key, data) {                
+            var context = {title: data.titulo, body: data.desc,foto:data.foto};
+            $('.result').append(template(context));                
+        })
+
+          //app.hideLoadingBar();
+        });
+
+    },
+    showLoadingBar:function(message){
+        //console.log("show loading bar");
+        $.mobile.loading( 'show', {
+            text: message,
+            textVisible: true,
+            theme: 'z',
+            html: ""
+        });             
+    },
+    hideLoadingBar:function(){
+        $.mobile.loading( 'hide', {});
+    },
+    loadRss:function(){
+        //var feed = 'http://www.frenterenovador.org.ar/feed/';
+        var feed = 'rss';
+        app.cargarNotas(feed);
+
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {    
+        app.hideLoadingBar();
+        this.loadRss("home");    
+        app.receivedEvent('deviceready');        
+    },    
+    onPageLoad:function(){   
+        
+        
+    },
+    receivedEvent: function(id) {
+        
+        //alert(id);
+        //$('#headerT').html("Cargado");
+    }
+};
